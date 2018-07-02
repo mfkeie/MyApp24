405aa47bc0940eec163c4ccbc5007a347f109675
uia <uia@olabank.ru>
2018-07-02 16:57:59 +0300

    first commit

---
+++ app/src/main/java/com/example/uia/myapp24/RecyclerFragment.java
@@ -0,0 +1,22 @@
+package com.example.uia.myapp24;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RecyclerFragment extends Fragment {
+
+    public static RecyclerFragment newInstance() {
+        return new RecyclerFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fr_recycler, container, false);
+    }
+}
