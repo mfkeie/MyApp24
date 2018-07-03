0d6cab33a1cd6d75893172246710f9a45a8bd399
urakov.ivan <urakov.ivan@gmail.com>
2018-07-03 06:56:18 +0300

    3 commit

--- app/src/main/java/com/example/uia/myapp24/MainActivity.java
+++ app/src/main/java/com/example/uia/myapp24/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.uia.myapp24;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -12,12 +14,24 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //getting the toolbar ^ https://www.simplifiedcoding.net/android-toolbar-example/#Creating-Toolbar-Layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setting the title
+        toolbar.setTitle("My Toolbar");
 
-        if(savedInstanceState == null) {
+        //placing toolbar in place of actionbar
+        setSupportActionBar(toolbar);
+
+      /*  if(savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.container, RecyclerFragment.newInstance())
                     .commit();
-        }
+        }*/
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 }
